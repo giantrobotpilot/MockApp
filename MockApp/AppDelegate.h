6c2cc4f1aea0bf78a53e623988519b405709ee5a
giantrobotpilot <drew@giantrobotpilot.com>
2013-09-26 11:56:47 -0600

    Initial Commit

---
+++ MockApp/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  MockApp
+//
+//  Created by Drew on 9/26/13.
+//  Copyright (c) 2013 Atoms-Express. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ MockApp/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  MockApp
+//
+//  Created by Drew on 9/26/13.
+//  Copyright (c) 2013 Atoms-Express. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
